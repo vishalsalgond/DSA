5e407a5cd58f6848fe0e729afd7ffa0cc2cf9631
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-11-30 01:13:53 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -21,6 +21,19 @@ typedef vector<pl>    vpl;
 typedef vector<vi>    vvi;
 typedef vector<vl>    vvl;
 const ll mod = 1e9 + 7;
+//Read array
+template <typename T>
+inline void readA(vector<T>& arr, int n) {
+	arr.resize(n);
+	for (auto& element : arr) cin >> element;
+}
+//Print array
+template <typename T>
+inline void printA(const vector<T> &arr) {
+	for(T ele : arr) cout << ele << " ";
+	cout << nl;
+}
+
 //LCM
 inline ll lcm(ll a, ll b) { return ((a / gcd(a, b)) * b); }
 //MODULO OPEARTIONS
@@ -29,28 +42,28 @@ inline ll sub(ll a, ll b) { return ((( (a % mod) - (b % mod)) % mod) + mod ) % m
 inline ll mul(ll a, ll b) { return ((( (a % mod) * (b % mod)) % mod) + mod ) % mod ; }
 //POWER
 ll fastpow(ll x, ll y) {
-  ll res = 1; x = x % mod;
-  while (y > 0) {
-    if (y & 1) res = (res * x) % mod;
-    y = y >> 1 ;
-    x = (x * x) % mod;
-  }
-  return (res % mod);
+	ll res = 1; x = x % mod;
+	while (y > 0) {
+		if (y & 1) res = (res * x) % mod;
+		y = y >> 1 ;
+		x = (x * x) % mod;
+	}
+	return (res % mod);
 }
 //MODULAR INVERSE (based on fermat's little theorem, works when m is prime)
 ll inv(ll x, ll m) { return fastpow(x , m - 2);}
 //MODULAR DIVISION (given numbers need not be co-prime)
 ll moddiv(ll a, ll b) {
-  ll temp = gcd(a, b);
-  a /= temp;
-  b /= temp;
-  return ((a * inv(b, mod)) % mod);
+	ll temp = gcd(a, b);
+	a /= temp;
+	b /= temp;
+	return ((a * inv(b, mod)) % mod);
 }
 //=========================  template ends here  ===========================
 
 void foo() {
-
-}
+  
+}	
 
 
 int main() {
@@ -62,7 +75,7 @@ int main() {
 #endif
   int T; cin >> T;
   for (int i = 1; i <= T; i++) {
-    foo();
+	foo();
   }
   return 0;
 }
